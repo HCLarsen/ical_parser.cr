ab91edcb6fad457b0366bf7af9da69a15e5728f9
Chris Larsen <clarsenipod@gmail.com>
2018-03-13 03:01:18 -0400

    Add Type Declarations to Parser

--- src/iCal/parser.cr
+++ src/iCal/parser.cr
@@ -3,18 +3,22 @@ class ICal::Parser
   UTC_TIME = Time::Format.new("%Y%m%dT%H%M%SZ")
   ZONED_TIME = Time::Format.new("%Y%m%dT%H%M%S")
 
+  DT_REGEX = /^\d{8}T\d{6}(?!Z)/
+  DT_UTC_REGEX = /^\d{8}T\d{6}Z/
+  DT_TZ_REGEX = /(?<=\w:)\d{8}T\d{6}/
+
   def parse_events(filename : String)
   end
 
-  def self.rfc5545_text_escape(string)
+  def self.rfc5545_text_escape(string) : String
     string.gsub(/(\,|\;|\\[^n])/){ |match| "\\" + match }
   end
 
-  def self.rfc5545_text_unescape(string)
+  def self.rfc5545_text_unescape(string) : String
     string.gsub(/(\\(?!\\))/){ |match| "" }
   end
 
-  def self.duration(string)
+  def self.duration(string) : Time::Span
     days = (/(\d+)(?=W)/.match(string).try &.[1].to_i || 0) * 7
     days += /(\d+)(?=D)/.match(string).try &.[1].to_i || 0
     hours = /(\d+)(?=H)/.match(string).try &.[1].to_i || 0
@@ -25,19 +29,15 @@ class ICal::Parser
   end
 
   def self.from_iCalDT(string) : Time
-    dTRegex = /^\d{8}T\d{6}(?!Z)/
-    dTUTCRegex = /^\d{8}T\d{6}Z/
-    dTTZRegex = /(?<=\w:)\d{8}T\d{6}/
-
-    if dTRegex.match(string)
+    if DT_REGEX.match(string)
       Time.parse(string, FLOATING_TIME.pattern, Time::Kind::Local)
-    elsif dTUTCRegex.match(string)
+    elsif DT_UTC_REGEX.match(string)
       Time.parse(string, UTC_TIME.pattern, Time::Kind::Utc)
-    elsif dTTZRegex.match(string)
+    elsif DT_TZ_REGEX.match(string)
       # Always matches a zoned time to local time. This will likely cover >90% of applications,
       # however, I will eventually need to include proper time zone parsing and usage. I may create
       # a time zone gem similar to Ruby's tzinfo.
-      timeString = (dTTZRegex.match(string) || ["0"])[0]
+      timeString = (DT_TZ_REGEX.match(string) || ["0"])[0]
       Time.parse(timeString, ZONED_TIME.pattern, Time::Kind::Local)
     else
       raise "Invalid Time Format"
