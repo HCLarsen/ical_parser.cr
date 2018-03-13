0d03db1f24680f2d3de80e8fdac025f9871130d0
Chris Larsen <clarsenipod@gmail.com>
2018-03-13 18:36:54 -0400

    Add Event Parsing to Parser

--- src/iCal/parser.cr
+++ src/iCal/parser.cr
@@ -1,7 +1,17 @@
 require "./event"
 
 class ICal::Parser
-  def self.parse_events(filename : String)
+  EVENT_REGEX = /(?s)(?<=BEGIN:VEVENT\n)(.*)(?=END:VEVENT)/
+
+  def self.parse_events(filename : String) : Array(Event)
     string = File.read(filename)
+    matches = EVENT_REGEX.match(string)
+    if matches
+      matches.captures.compact.map do |match|
+        Event.new(match)
+      end
+    else
+      [] of Event
+    end
   end
 end
--- test/parser_test.cr
+++ test/parser_test.cr
@@ -6,6 +6,8 @@ class ParserTest < Minitest::Test
   def test_should_parse_events_from_iCal
     filename = File.join(File.dirname(__FILE__), "files", "test.ics")
     events = ICal::Parser.parse_events(filename)
-    puts events
+    assert_equal events.class, Array(ICal::Event)
+    assert_equal events.size, 1
+    assert_equal events.first.summary, "Lunchtime meeting"
   end
 end
