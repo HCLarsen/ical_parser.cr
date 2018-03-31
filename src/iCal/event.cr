8aa8f0fac08e1d194c9e2d77e942734e5642fdd2
Chris Larsen <clarsenipod@gmail.com>
2018-03-30 22:09:47 -0400

    Strip whitespace from regex matches

--- src/iCal/event.cr
+++ src/iCal/event.cr
@@ -3,48 +3,48 @@ require "./common"
 class ICal::Event
   getter summary : String
   getter uid : String
-  getter start : Time
-  getter end : Time
+  getter dtstart : Time
+  getter dtend : Time
   getter description : String?
   getter location : String?
   getter url : String?
 
   def initialize(vevent : String)
-    summaryRegex = /(?<=SUMMARY:)(.*)(?=\n)/
-    uidRegex = /(?<=UID:)(.*)(?=\n)/
+    summaryRegex = /SUMMARY:(.*)\R/
+    uidRegex = /UID:(.*)\R/
 
-    descriptionRegex = /(?s)(?<=DESCRIPTION:)(.*?)(?=\n\w)/
-    locationRegex = /(?<=LOCATION:)(.*)(?=\n)/
-    urlRegex = /(?<=URL:)(.*)(?=\n)/
+    descriptionRegex = /(?s)DESCRIPTION:(.*?)\R\w/
+    locationRegex = /(?<=LOCATION:)(.*)\R/
+    urlRegex = /URL:(.*)\R/
 
-    startRegex = /(?<=DTSTART).*:(.*)(?=\n)/
-    endRegex = /(?<=DTEND).*:(.*)(?=\n)/
-    durationRegex = /(?<=DURATION).*:(.*)(?=\n)/
+    startRegex = /DTSTART.*:(.*)\R/
+    endRegex = /DTEND.*:(.*)\R/
+    durationRegex = /DURATION.*:(.*)\R/
 
-    @summary = summaryRegex.match(vevent).try &.[1] || ""
-    @uid = uidRegex.match(vevent).try &.[1] || ""
-    startString = startRegex.match(vevent).try &.[1] || ""
-    @start = ICal.from_iCalDT(startString)
-    if endRegex.match(vevent)
-      endString = endRegex.match(vevent).try &.[1] || ""
-      @end = ICal.from_iCalDT(endString)
-    elsif durationRegex.match(vevent)
-      durationString = durationRegex.match(vevent).try &.[1] || ""
-      duration = ICal.duration(durationString)
-      @end = @start + duration
+    @summary = summaryRegex.match(vevent).try &.[1].strip || ""
+    @uid = uidRegex.match(vevent).try &.[1].strip || ""
+    @location = locationRegex.match(vevent).try &.[1].strip
+
+    if startString = startRegex.match(vevent)
+      @dtstart = ICal.from_iCalDT(startString[1].strip)
+    else
+      raise "Invalid Event: No start time found"
+    end
+
+    if endString = endRegex.match(vevent)
+      @dtend = ICal.from_iCalDT(endString[1].strip)
+    elsif durationString = durationRegex.match(vevent)
+      duration = ICal.duration(durationString[1].strip)
+      @dtend = @dtstart + duration
     else
       # For cases where a "VEVENT" calendar component specifies a "DTSTART" property with a DATE value type but no "DTEND" nor "DURATION" property, the event's duration is taken to be one day.  For cases where a "VEVENT" calendar component specifies a "DTSTART" property with a DATE-TIME value type but no "DTEND" property, the event ends on the same calendar date and time of day specified by the "DTSTART" property.
       raise "No End Time or Duration Found"
     end
 
-    descriptionString = descriptionRegex.match(vevent).try &.[1] || nil
-    if descriptionString
-      @description = ICal.rfc5545_text_unescape(descriptionString)
-    else
-      @description = nil
+    if descriptionString = descriptionRegex.match(vevent)
+      @description = ICal.rfc5545_text_unescape(descriptionString[1].strip)
     end
 
-    @location = locationRegex.match(vevent).try &.[1] || nil
-    @url = urlRegex.match(vevent).try &.[1] || nil
+    @url = urlRegex.match(vevent).try &.[1].strip || nil
   end
 end
--- test/event_test.cr
+++ test/event_test.cr
@@ -1,8 +1,10 @@
 require "minitest/autorun"
 
-require "/../src/ICal/event"
+require "/../src/ICal/parser"
+require "./ics_stream"
 
 class EventTest < Minitest::Test
+
   def test_parses_event_with_duration
     event_string = <<-HEREDOC
     BEGIN:VEVENT
@@ -12,7 +14,7 @@ class EventTest < Minitest::Test
     DURATION:PT1H
     DESCRIPTION: We'll continue with the unfinished business from last time,
      in particular:
-       Can names start with a number?
+       Can names dtstart with a number?
        What if they are all numeric?
        Reuse of names - is it valid
      I remind the attendees we have spent 3 months on these subjects. We need
@@ -22,9 +24,9 @@ class EventTest < Minitest::Test
     HEREDOC
 
     descriptionString = <<-DESCRIPTION_STRING
-     We'll continue with the unfinished business from last time,
+    We'll continue with the unfinished business from last time,
      in particular:
-       Can names start with a number?
+       Can names dtstart with a number?
        What if they are all numeric?
        Reuse of names - is it valid
      I remind the attendees we have spent 3 months on these subjects. We need
@@ -34,8 +36,8 @@ class EventTest < Minitest::Test
     event = ICal::Event.new(event_string)
     assert_equal "Lunchtime meeting", event.summary
     assert_equal "ff808181-1fd7389e-011f-d7389ef9-00000003", event.uid
-    assert_equal Time.new(2016, 4, 20, 12, 0, 0, kind: Time::Kind::Local), event.start
-    assert_equal Time.new(2016, 4, 20, 13, 0, 0, kind: Time::Kind::Local), event.end
+    assert_equal Time.new(2016, 4, 20, 12, 0, 0, kind: Time::Kind::Local), event.dtstart
+    assert_equal Time.new(2016, 4, 20, 13, 0, 0, kind: Time::Kind::Local), event.dtend
     assert_equal "Mo's bar - back room", event.location
     assert_equal descriptionString, event.description
   end
@@ -54,7 +56,23 @@ class EventTest < Minitest::Test
     HEREDOC
     event = ICal::Event.new(event_string)
     assert_equal "Annual Employee Review", event.summary
-    assert_equal Time.new(1997, 9, 3, 16, 30, 0, nanosecond: 0, kind: Time::Kind::Utc), event.start
-    assert_equal Time.new(1997, 9, 3, 19, 0, 0, nanosecond: 0, kind: Time::Kind::Utc), event.end
+    assert_equal Time.new(1997, 9, 3, 16, 30, 0, kind: Time::Kind::Utc), event.dtstart
+    assert_equal Time.new(1997, 9, 3, 19, 0, 0, kind: Time::Kind::Utc), event.dtend
+    assert event.description.nil?
+    assert event.location.nil?
   end
+
+  def test_parses_facebook_event
+    event = ICal::Event.new(Fixture.facebook_event)
+    assert_equal "OMD", event.summary
+    assert_equal "https://www.facebook.com/events/122768155051111/", event.url
+    assert_equal Time.new(2018, 3, 13, 23, 0, 0, kind: Time::Kind::Utc), event.dtstart
+    assert_equal "The Danforth Music Hall", event.location
+  end
+
+#  def test_parses_attendees
+#    event = ICal::Event.new(Fixture.facebook_event)
+#    puts event.size
+#    puts event[0]
+#  end
 end
