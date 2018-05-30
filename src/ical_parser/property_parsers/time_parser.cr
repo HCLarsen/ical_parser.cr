ac016a547faa3693fb307234353e4a72af5b6a44
Chris Larsen <clarsenipod@gmail.com>
2018-05-30 13:58:20 -0400

    Add UTC parsing to TimeParser

--- src/ical_parser/property_parsers/time_parser.cr
+++ src/ical_parser/property_parsers/time_parser.cr
@@ -1,9 +1,19 @@
 module IcalParser
   class TimeParser < ValueParser
     TIME = Time::Format.new("%H%M%S")
-    
+    UTC_TIME = Time::Format.new("%H%M%SZ")
+
+    DT_FLOATING_REGEX = /^\d{6}$/
+    DT_UTC_REGEX = /^\d{6}Z$/
+
     def parse(string : String)
-      Time.parse(string, TIME.pattern)
+      if DT_FLOATING_REGEX.match(string)
+        Time.parse(string, TIME.pattern, Time::Kind::Local)
+      elsif DT_UTC_REGEX.match(string)
+        Time.parse(string, UTC_TIME.pattern, Time::Kind::Utc)
+      else
+        raise "Invalid Time format"
+      end
     end
   end
 end
--- test/property_parsers/time_parser_test.cr
+++ test/property_parsers/time_parser_test.cr
@@ -12,6 +12,18 @@ class TimeParserTest < Minitest::Test
 
   def test_parses_time
     time = "230000"
-    assert_equal Time.new(1,1,1,23,0,0), @parser.parse(time)
+    assert_equal Time.new(1,1,1,23,0,0, nanosecond: 0, kind: Time::Kind::Local), @parser.parse(time)
+  end
+
+  def test_parses_utc_time
+    time = "070000Z"
+    assert_equal Time.utc(1,1,1,7,0,0), @parser.parse(time)
+  end
+
+  def test_raises_invalid_time_format
+    error = assert_raises do
+      @parser.parse("230000-0800")
+    end
+    assert_equal "Invalid Time format", error.message
   end
 end
