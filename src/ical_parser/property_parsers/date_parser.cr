3bdaefa6b84a254aa6d3241919def66ed913fc4f
Chris Larsen <clarsenipod@gmail.com>
2018-05-30 12:39:56 -0400

    Improve DateParser

--- src/ical_parser/property_parsers/date_parser.cr
+++ src/ical_parser/property_parsers/date_parser.cr
@@ -2,11 +2,10 @@ require "./value_parser"
 
 module IcalParser
   class DateParser < ValueParser
+    DATE = Time::Format.new("%Y%m%d")
+
     def parse(string : String) : Time
-      year = /(\d{4})/.match(string).try &.[1].to_i || 0
-      month = /\d{4}(\d{2})/.match(string).try &.[1].to_i || 0
-      day = /\d{6}(\d{2})/.match(string).try &.[1].to_i || 0
-      Time.new(year, month, day)
+      Time.parse(string, DATE.pattern)
     end
   end
 end
