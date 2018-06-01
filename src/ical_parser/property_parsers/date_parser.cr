f1789874f825e724cc27677dbed90916cd986fa6
Chris Larsen <clarsenipod@gmail.com>
2018-06-01 13:24:04 -0400

    Add error checking to DateParser

--- src/ical_parser/property_parsers/date_parser.cr
+++ src/ical_parser/property_parsers/date_parser.cr
@@ -3,17 +3,26 @@ require "./value_parser"
 module IcalParser
   class DateParser < ValueParser
     DATE = Time::Format.new("%Y%m%d")
+    DATE_REGEX = /^\d{8}$/
 
     def parse(string : String, params = {} of String => String) : Time
-      if params["kind"]? == "Local"
-        kind = Time::Kind::Local
-      elsif params["kind"]? == "Utc"
-        kind = Time::Kind::Utc
-      else
-        kind = Time::Kind::Unspecified
-      end
+      if DATE_REGEX.match(string)
+        if params["kind"]? == "Local"
+          kind = Time::Kind::Local
+        elsif params["kind"]? == "Utc"
+          kind = Time::Kind::Utc
+        else
+          kind = Time::Kind::Unspecified
+        end
 
-      Time.parse(string, DATE.pattern, kind)
+        begin
+          Time.parse(string, DATE.pattern, kind)
+        rescue
+          raise "Invalid Date"
+        end
+      else
+        raise "Invalid Date format"
+      end
     end
   end
 end
--- test/property_parsers/date_parser_test.cr
+++ test/property_parsers/date_parser_test.cr
@@ -14,4 +14,18 @@ class DateParserTest < Minitest::Test
     date = "19970714"
     assert_equal Time.new(1997,7,14), @parser.parse(date)
   end
+
+  def test_raises_for_invalid_date_format
+    error = assert_raises do
+      @parser.parse("970714")
+    end
+    assert_equal "Invalid Date format", error.message
+  end
+
+  def test_raises_for_invalid_date
+    error = assert_raises do
+      @parser.parse("19970740")
+    end
+    assert_equal "Invalid Date", error.message
+  end
 end
