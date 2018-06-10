7231f532b50e0c7babc72f59f8c7a9ec37f0559f
Chris Larsen <clarsenipod@gmail.com>
2018-06-10 03:06:24 -0400

    Improve documentation on TimeParser

--- src/ical_parser/property_parsers/time_parser.cr
+++ src/ical_parser/property_parsers/time_parser.cr
@@ -1,12 +1,24 @@
 module IcalParser
-  # Parser for the [Time](https://tools.ietf.org/html/rfc5545#section-3.3.12) value type
+  # The TimeParser singleton class parses the RFC5545 [Time](https://tools.ietf.org/html/rfc5545#section-3.3.12) value type
   #
-  # > Note: The Time parser does not take time zone information into account,
+  # As a singleton class, typical instantiation will result in a compile
+  # time error.
+  #
+  # ```
+  # TimeParser.new  #=> private method 'new' called for IcalParser::TimeParser:Class
+  # ```
+  #
+  # Instead, access the singleton instance by calling #parser on the class.
+  #
+  # ```
+  # parser = TimeParser.parser  #=> #<IcalParser::TimeParser:0x1062d0f60>
+  # ```
+  #
+  # NOTE: The Time parser does not take time zone information into account,
   # despite the RFC specification listing it as a form for Time values.
   # This is due to the impact of Daylight Savings Time, which makes it
   # impossible to determine the offset of a time zone without knowing the date.
   # Time zone information is instead parsed and set by the Date-Time parser.
-  #
   class TimeParser < ValueParser(Time)
     TIME = Time::Format.new("%H%M%S")
     UTC_TIME = Time::Format.new("%H%M%SZ")
@@ -16,6 +28,9 @@ module IcalParser
 
     # Parses the Time value and returns it as a Crystal Time object.
     #
+    # ```
+    # TimeParser.parser.parse("230000") #=> 0001-01-01 23:00:00
+    # ```
     def parse(string : String, params = {} of String => String) : T
       if DT_FLOATING_REGEX.match(string)
         Time.parse(string, TIME.pattern, Time::Kind::Unspecified)
