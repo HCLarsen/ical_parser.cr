f17d2a2f0ece6b3fae89cfcbfd4d0fd07184b21b
Chris Larsen <clarsenipod@gmail.com>
2018-06-14 17:36:30 -0400

    Fixed property tests

--- test/property_test.cr
+++ test/property_test.cr
@@ -7,19 +7,6 @@ class PropertyTest < Minitest::Test
 
   def initialize(argument)
     super(argument)
-    @eventc = <<-HEREDOC
-    BEGIN:VEVENT
-    DTSTAMP:19960704T120000Z
-    UID:uid1@example.com
-    ORGANIZER:mailto:jsmith@example.com
-    DTSTART:19960918T143000Z
-    DTEND:19960920T220000Z
-    STATUS:CONFIRMED
-    CATEGORIES:CONFERENCE
-    SUMMARY:Networld+Interop Conference
-    DESCRIPTION:Networld+Interop Conference and Exhibit\nAtlanta World Congress Center\nAtlanta\, Georgia
-    END:VEVENT
-    HEREDOC
   end
 
   def test_property_has_name
@@ -46,8 +33,21 @@ class PropertyTest < Minitest::Test
   end
 
   def test_property_gets_value
+    eventc = <<-HEREDOC
+    BEGIN:VEVENT
+    DTSTAMP:19960704T120000Z
+    UID:uid1@example.com
+    ORGANIZER:mailto:jsmith@example.com
+    DTSTART:19960918T143000Z
+    DTEND:19960920T220000Z
+    STATUS:CONFIRMED
+    CATEGORIES:CONFERENCE
+    SUMMARY:Networld+Interop Conference
+    DESCRIPTION:Networld+Interop Conference and Exhibit\nAtlanta World Congress Center\nAtlanta\, Georgia
+    END:VEVENT
+    HEREDOC
     property = Property.new("UID", TextParser.parser)
-    uid = property.get(@eventc)
+    uid = property.get(eventc)
     assert_equal "uid1@example.com", uid
   end
 
