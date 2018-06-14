6a149437e8df33cf6876ffe133bd268695a5ed66
Chris Larsen <clarsenipod@gmail.com>
2018-06-14 00:54:14 -0400

    Add documentation for TextParser

--- src/ical_parser/property_parsers/text_parser.cr
+++ src/ical_parser/property_parsers/text_parser.cr
@@ -1,6 +1,21 @@
 require "./value_parser"
 
 module IcalParser
+  # The TextParser singleton class parses the RFC5545 [Text](https://tools.ietf.org/html/rfc5545#section-3.3.11) value type
+  #
+  # As a singleton class, typical instantiation will result in a compile
+  # time error.
+  #
+  # ```
+  # TextParser.new  #=> private method 'new' called for IcalParser::TextParser:Class
+  # ```
+  #
+  # Instead, access the singleton instance by calling #parser on the class.
+  #
+  # ```
+  # parser = TextParser.parser  #=> #<IcalParser::TextParser:0x1062d0f60>
+  # ```
+  #
   class TextParser < ValueParser(String)
     def parse(string : String, params = {} of String => String) : T
       string.gsub(/(\\(?!\\))/){ |match| "" }
