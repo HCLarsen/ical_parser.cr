36bc711dab2c397c8c611bee4686525288e81049
Chris Larsen <clarsenipod@gmail.com>
2018-07-12 17:28:51 -0400

    Refactor ICSStream

--- src/ical_parser/ics_stream.cr
+++ src/ical_parser/ics_stream.cr
@@ -4,50 +4,44 @@ module IcalParser
   class ICSStream
     def self.read(filename : String) : Calendar
       stream = File.read(filename)
-      CalendarParser.parser.parse(stream)
+      parse(stream).first
     end
 
     def self.read(address : URI) : Calendar
-      if address.scheme == "webcal"
-        address.scheme = "https"
-      end
-      response = HTTP::Client.get address
+      response = HTTP::Client.get make_https(address)
       stream = response.body
-      CalendarParser.parser.parse(stream)
+      parse(stream).first
     end
 
     def self.read_calendars(filename : String) : Array(Calendar)
       stream = File.read(filename)
-      parse_multiple(stream)
+      parse(stream)
     end
 
     def self.read_calendars(address : URI) : Array(Calendar)
-      if address.scheme == "webcal"
-        address.scheme = "https"
-      end
-      response = HTTP::Client.get address
+      response = HTTP::Client.get make_https(address)
       stream = response.body
-      parse_multiple(stream)
+      parse(stream)
     end
 
-    private def self.parse_single(stream : String) : Calendar
-      object_regex = /^BEGIN:VCALENDAR.*?END:VCALENDAR/m
-
-      if calendar_object = stream.match(object_regex)
-        CalendarParser.parser.parse(calendar_object[0])
-      else
-        raise "No calendar objects found in stream"
-      end
-
-    end
-
-    private def self.parse_multiple(stream : String) : Array(Calendar)
+    private def self.parse(stream : String) : Array(Calendar)
       object_regex = /^BEGIN:VCALENDAR.*?END:VCALENDAR/m
 
       calendar_objects = stream.scan(object_regex)
+
+      if calendar_objects.size == 0
+        raise "No calendar objects found in stream"
+      end
+
       calendar_objects.map do |calendar_object|
         CalendarParser.parser.parse(calendar_object[0])
       end
+    end
+
+    private def self.make_https(address : URI)
+      address.scheme = "https" if address.scheme == "webcal"
+
+      return address
     end
   end
 end
