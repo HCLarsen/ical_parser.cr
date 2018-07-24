99aa1605f1beeeacfb7ea7636e45bb50536630b3
Chris Larsen <clarsenipod@gmail.com>
2018-07-24 16:39:03 -0400

    Improve alias creation macro in top level

--- src/iCal.cr
+++ src/iCal.cr
@@ -5,16 +5,15 @@ require "./ical_parser/property_parsers/*"
 
 # TODO: Write documentation for `IcalParser`
 module IcalParser
-  macro create_aliases
-    {% types = [Bool, CalAddress, Float64, Int32, PeriodOfTime, String, Time, Time::Span, URI] %}
-    alias ValueType = {{ types.join(" | ").id }}
-    {% arrays = types.map{|e| "Array(#{e})".id } %}
+  TYPES = [Bool, CalAddress, Float64, Int32, PeriodOfTime, String, Time, Time::Span, URI]
+
+  {% begin %}
+    alias ValueType = {{ TYPES.join(" | ").id }}
+    {% arrays = TYPES.map{|e| "Array(#{e})".id } %}
     alias ValueArray = {{ arrays.join(" | ").id }}
-    {% hashes = types.map{|e| "Hash(String, #{e})".id } %}
+    {% hashes = TYPES.map{|e| "Hash(String, #{e})".id } %}
     alias ValueHash = {{ hashes.join(" | ").id }}
 
     alias PropertyType = ValueType | ValueArray | ValueHash
-  end
-
-  create_aliases
+  {% end %}
 end
