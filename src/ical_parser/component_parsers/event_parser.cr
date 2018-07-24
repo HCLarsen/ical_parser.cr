a82d6fc2d2ba4a619b8dc1eae0b620f38c4e020a
Chris Larsen <clarsenipod@gmail.com>
2018-07-24 18:39:44 -0400

    Further refactor EventParser#parse method

--- src/ical_parser/component_parsers/event_parser.cr
+++ src/ical_parser/component_parsers/event_parser.cr
@@ -1,5 +1,24 @@
 module IcalParser
   class EventParser
+    COMPONENT_PROPERTIES = {
+      "uid"             => Property.new(TextParser.parser),
+      "dtstamp"         => Property.new(DateTimeParser.parser),
+      "dtstart"         => Property.new(DateTimeParser.parser),
+      "dtend"           => Property.new(DateTimeParser.parser),
+      "duration"        => Property.new(DurationParser.parser),
+      "summary"         => Property.new(TextParser.parser),
+      "description"     => Property.new(TextParser.parser),
+      "classification"  => Property.new(TextParser.parser),
+      "categories"      => Property.new(TextParser.parser, single_value: false, only_once: false),
+      "transp"          => Property.new(TextParser.parser),
+      "status"          => Property.new(TextParser.parser),
+      "location"        => Property.new(TextParser.parser),
+      "sequence"        => Property.new(IntegerParser.parser),
+      "organizer"       => Property.new(CalAddressParser.parser),
+      "attendees"       => Property.new(CalAddressParser.parser, only_once: false),
+      "geo"             => Property.new(FloatParser.parser, parts: ["lat", "lon"]),
+    }
+
     private def initialize; end
 
     def self.parser : EventParser
@@ -11,59 +30,36 @@ module IcalParser
     end
 
     def parse(eventc : String)
-      component_properties = {
-        "uid"             => Property.new(TextParser.parser),
-        "dtstamp"         => Property.new(DateTimeParser.parser),
-        "dtstart"         => Property.new(DateTimeParser.parser),
-        "dtend"           => Property.new(DateTimeParser.parser),
-        "duration"        => Property.new(DurationParser.parser),
-        "summary"         => Property.new(TextParser.parser),
-        "description"     => Property.new(TextParser.parser),
-        "classification"  => Property.new(TextParser.parser),
-        "categories"      => Property.new(TextParser.parser, single_value: false, only_once: false),
-        "transp"          => Property.new(TextParser.parser),
-        "status"          => Property.new(TextParser.parser),
-        "location"        => Property.new(TextParser.parser),
-        "sequence"        => Property.new(IntegerParser.parser),
-        "organizer"       => Property.new(CalAddressParser.parser),
-        "attendees"       => Property.new(CalAddressParser.parser, only_once: false),
-        "geo"             => Property.new(FloatParser.parser, parts: ["lat", "lon"]),
-      }
       all_day = false
       found = Hash(String, PropertyType).new
-      regex = /(?<name>.*?)(?<params>;.*?)?:(?<value>.*)/
 
-      eventc = remove_first_and_last_lines(eventc)
+      lines = content_lines(eventc)
 
-      lines = eventc.lines
-      lines.each do |line|
-        if match = line.match(regex)
-          name = match["name"].downcase
-          name = "classification" if name == "class"
-          name = "attendees" if name == "attendee"
-          all_day = true if name == "dtstart" && match["params"]? && match["params"].match(/VALUE=DATE(?:$|[^-])/)
+      matches = lines_matches(lines)
 
-          if component_properties.keys.includes? name
-            property = component_properties[name]
-            value = property.parse(match["value"], match["params"]?)
+      matches.each do |match|
+        name = match["name"].downcase
+        name = "classification" if name == "class"
+        name = "attendees" if name == "attendee"
 
-            unless found[name]?
-              found[name] = value
+        if name == "dtstart" && match["params"]?
+          params = match["params"].lstrip(';').split((";"))
+          all_day = true if params.includes?("VALUE=DATE")
+        end
+
+        if COMPONENT_PROPERTIES.keys.includes? name
+          property = COMPONENT_PROPERTIES[name]
+          value = property.parse(match["value"], match["params"]?)
+
+          unless found[name]?
+            found[name] = value
+          else
+            if property.only_once && property.single_value
+            elsif !property.only_once
+              found[name] = new_array
             else
-              if property.only_once && property.single_value
-              elsif !property.only_once
-                case value
-                when Array(CalAddress)
-                  found[name] = found[name].as Array(CalAddress) + value
-                when Array(String)
-                  found[name] = found[name].as Array(String) + value
-                end
-              else
-              end
             end
           end
-        else
-          raise "Invalid Event: No value on line #{line}"
         end
       end
 
@@ -74,11 +70,34 @@ module IcalParser
       return event
     end
 
-    private def remove_first_and_last_lines(string : String)
-      lines = string.lines
+    private def content_lines(component : String)
+      lines = component.lines
       lines.shift?
       lines.pop?
-      lines.join("\r\n")
+      lines
+    end
+
+    private def lines_matches(lines : Array(String))
+      regex = /(?<name>.*?)(?<params>;.*?)?:(?<value>.*)/
+
+      lines.map do |line|
+        if match = line.match(regex)
+          match
+        else
+          raise "Invalid Event: No value on line #{line}"
+        end
+      end
+    end
+
+    private macro new_array
+      case value
+      {% for type in TYPES %}
+      when Array({{type.id}})
+        found[name].as Array({{type.id}}) + value
+      {% end %}
+      else
+        raise "Invalid property type"
+      end
     end
 
     private def validate(data, all_day : Bool)
