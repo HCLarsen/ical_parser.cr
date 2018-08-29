6d0db60903e9e3587dd6776fa7f6c1b81e62db24
Chris Larsen <clarsenipod@gmail.com>
2018-08-29 18:38:05 -0400

    Add parsing of basic recurrences

--- src/ical_parser/property_parsers/recurrance_rule_parser.cr
+++ src/ical_parser/property_parsers/recurrance_rule_parser.cr
@@ -14,5 +14,27 @@ module IcalParser
   # recur.week_start  #=> Time::DayOfWeek::Sunday
   # recur.by_day      #=> [Time::DayOfWeek::Tuesday, Time::DayOfWeek::Thursday]
   class RecurranceRuleParser < ValueParser(RecurranceRule)
+    def parse(string : String, params = {} of String => String) : T
+      hash = {} of String => Array(String)
+      pairs = string.split(';')
+      pairs.each do  |pair|
+        name, value = pair.split('=').map(&.downcase)
+        hash[name] = value.split(',')
+      end
+
+      frequency = RecurranceRule::Freq.from_string(hash["freq"].first)
+      interval = hash["interval"]? ? hash["interval"].first.to_i : 1
+
+      if hash["until"]?
+        until_string = hash["until"].first.upcase
+        end_time = DateTimeParser.parser.parse(until_string)
+        RecurranceRule.new(frequency, end_time: end_time, interval: interval)
+      elsif hash["count"]?
+        count = hash["count"].first.to_i
+        RecurranceRule.new(frequency, count: count, interval: interval)
+      else
+        RecurranceRule.new(frequency, interval: interval)
+      end
+    end
   end
 end
--- test/property_parsers/recurrance_rule_parser_test.cr
+++ test/property_parsers/recurrance_rule_parser_test.cr
@@ -9,4 +9,26 @@ class RecurranceRuleParserTest < Minitest::Test
     super(arg)
     @parser = RecurranceRuleParser.parser
   end
+
+  def test_daily_with_count
+    string = "FREQ=DAILY;COUNT=10"
+    recur = @parser.parse(string)
+    assert_equal 10, recur.count
+    assert_equal RecurranceRule::Freq::Daily, recur.frequency
+    assert_equal 1, recur.interval
+  end
+
+  def test_daily_with_until
+    string = "FREQ=DAILY;UNTIL=19971224T000000Z"
+    recur = @parser.parse(string)
+    assert_equal Time.utc(1997, 12, 24), recur.end_time
+    assert_equal 1, recur.interval
+  end
+
+  def test_daily_with_interval
+    string = "FREQ=DAILY;INTERVAL=2"
+    recur = @parser.parse(string)
+    assert_equal RecurranceRule::Freq::Daily, recur.frequency
+    assert_equal 2, recur.interval
+  end
 end
