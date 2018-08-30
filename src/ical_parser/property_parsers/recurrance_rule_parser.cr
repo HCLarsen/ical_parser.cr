15cbcf97939f44f14477d60748a603c54b4549b9
Chris Larsen <clarsenipod@gmail.com>
2018-08-30 16:09:06 -0400

    Add BYMONTH and BYDAY parsing to RecurranceRuleParser

--- src/ical_parser/property_parsers/recurrance_rule_parser.cr
+++ src/ical_parser/property_parsers/recurrance_rule_parser.cr
@@ -15,25 +15,53 @@ module IcalParser
   # recur.by_day      #=> [Time::DayOfWeek::Tuesday, Time::DayOfWeek::Thursday]
   class RecurrenceRuleParser < ValueParser(RecurrenceRule)
     def parse(string : String, params = {} of String => String) : T
-      hash = {} of String => Array(String)
+      hash = {} of String => String
       pairs = string.split(';')
       pairs.each do  |pair|
-        name, value = pair.split('=').map(&.downcase)
-        hash[name] = value.split(',')
+        name, value = pair.split('=')
+        hash[name.downcase] = value
       end
 
-      frequency = RecurrenceRule::Freq.from_string(hash["freq"].first)
-      interval = hash["interval"]? ? hash["interval"].first.to_i : 1
+      frequency = RecurrenceRule::Freq.from_string(hash["freq"].downcase)
+      interval = hash["interval"]? ? hash["interval"].to_i : 1
+
+      rules = {} of String => RecurrenceRule::ByRuleType
+      rules["by_month"] = hash["bymonth"].split(',').map(&.to_i) if hash["bymonth"]?
+      if hash["byday"]?
+        days = hash["byday"].split(',').map { |day| day_to_day_of_week(day)}
+        rules["by_day"] = days.map { |day| {0, day} }
+      end
 
       if hash["until"]?
-        until_string = hash["until"].first.upcase
+        until_string = hash["until"].upcase
         end_time = DateTimeParser.parser.parse(until_string)
-        RecurrenceRule.new(frequency, end_time: end_time, interval: interval)
+        RecurrenceRule.new(frequency, end_time: end_time, by_rules: rules, interval: interval)
       elsif hash["count"]?
-        count = hash["count"].first.to_i
+        count = hash["count"].to_i
         RecurrenceRule.new(frequency, count: count, interval: interval)
       else
         RecurrenceRule.new(frequency, interval: interval)
+      end
+    end
+
+    private def day_to_day_of_week(day : String) : Time::DayOfWeek
+      case day
+      when "MO"
+        Time::DayOfWeek::Monday
+      when "TU"
+        Time::DayOfWeek::Tuesday
+      when "WE"
+        Time::DayOfWeek::Wednesday
+      when "TH"
+        Time::DayOfWeek::Thursday
+      when "FR"
+        Time::DayOfWeek::Friday
+      when "SA"
+        Time::DayOfWeek::Saturday
+      when "SU"
+        Time::DayOfWeek::Sunday
+      else
+        raise "Invalid Day of Week value"
       end
     end
   end
--- test/property_parsers/recurrance_rule_parser_test.cr
+++ test/property_parsers/recurrance_rule_parser_test.cr
@@ -31,4 +31,12 @@ class RecurrenceRuleParserTest < Minitest::Test
     assert_equal RecurrenceRule::Freq::Daily, recur.frequency
     assert_equal 2, recur.interval
   end
+
+  def test_yearly_by_month_by_day
+    string = "FREQ=YEARLY;UNTIL=20000131T140000Z;BYMONTH=1;BYDAY=SU,MO,TU,WE,TH,FR,SA"
+    sundays = {0, Time::DayOfWeek::Sunday}
+    recur = @parser.parse(string)
+    assert_equal [1], recur.by_month
+    assert_equal sundays, recur.by_day.not_nil!.first
+  end
 end
