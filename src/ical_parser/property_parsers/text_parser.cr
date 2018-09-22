6ece01366f16528b9dbbe808192511efd365b089
Chris Larsen <clarsenipod@gmail.com>
2018-09-22 15:06:42 -0400

    Add documentation to text parser and generator

--- src/ical_parser/property_parsers/text_parser.cr
+++ src/ical_parser/property_parsers/text_parser.cr
@@ -26,10 +26,12 @@ module IcalParser
     end
   end
 
+  # Parses a TEXT property into a String object, removing escape characters.
   @@text_parser = Proc(String, Hash(String, String), String).new do |value, params|
     value.gsub(/\\(?![nN\\])/) { |match| "" }
   end
 
+  # Converts a String object into a TEXT property, adding escape characters as needed.
   @@text_generator = Proc(String, String).new do |value|
     value.gsub(/(\,|\;|\\[^n])/) { |match| "\\" + match }
   end
