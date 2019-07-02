c30395b9ce5d09043d93821f5fe4f3f9c9b1e97c
Chris Larsen <clarsendev@protonmail.com>
2019-07-01 20:25:13 -0400

    Revise DateTimeParser to return string representation of time

--- src/ical_parser/property_parsers/date_time_parser.cr
+++ src/ical_parser/property_parsers/date_time_parser.cr
@@ -2,16 +2,11 @@ require "./date_parser"
 require "./time_parser"
 
 module IcalParser
-  @@date_time_parser = Proc(String, Hash(String, String), Time).new do |value, params|
-    begin
-      date_string, time_string = value.split('T')
-    rescue
+  @@date_time_parser = Proc(String, Hash(String, String), String).new do |value, params|
+    if DT_FLOATING_REGEX.match(value) || DT_UTC_REGEX.match(value)
+      value
+    else
       raise "Invalid Date-Time format"
     end
-    raise "Invalid Date-Time format" if date_string.empty? || time_string.empty?
-    time = @@time_parser.call(time_string, params)
-    params["TZID"] ||= time.location.to_s
-    date = @@date_parser.call(date_string, params)
-    date + time.time_of_day
   end
 end
--- test/property_parsers/date_time_parser_test.cr
+++ test/property_parsers/date_time_parser_test.cr
@@ -1,11 +1,13 @@
 require "minitest/autorun"
 
-require "/../src/iCal"
+require "/../src/ical_parser/property_parsers/date_time_parser"
+require "/../src/ical_parser/common"
+# require "/../src/iCal"
 
 class DateTimeParserTest < Minitest::Test
   include IcalParser
 
-  @parser : Proc(String, Hash(String, String), Time)
+  @parser : Proc(String, Hash(String, String), String)
 
   def initialize(arg)
     super(arg)
@@ -16,23 +18,20 @@ class DateTimeParserTest < Minitest::Test
   def test_parses_floating_date_time
     string = "19980118T230000"
     dateTime = @parser.call(string, @params)
-    assert_equal Time.new(1998, 1, 18, 23, 0, 0), dateTime
-    assert_equal Time::Location.local, dateTime.location
+    assert_equal string, dateTime
   end
 
   def test_parses_utc_date_time
     string = "19980119T070000Z"
     dateTime = @parser.call(string, @params)
-    assert_equal Time.utc(1998, 1, 19, 7, 0, 0), dateTime
-    assert_equal Time::Location::UTC, dateTime.location
+    assert_equal string, dateTime
   end
 
   def test_parses_date_time_with_time_zone
     string = "19970714T133000"
     params = {"TZID" => "America/New_York"}
     dateTime = @parser.call(string, params)
-    assert_equal Time.utc(1997, 7, 14, 17, 30, 0), dateTime
-    assert_equal Time::Location.load("America/New_York"), dateTime.location
+    assert_equal string, dateTime
   end
 
   def test_raises_for_invalid_time_format
@@ -40,7 +39,7 @@ class DateTimeParserTest < Minitest::Test
     error = assert_raises do
       @parser.call(string, @params)
     end
-    assert_equal "Invalid Time format", error.message
+    assert_equal "Invalid Date-Time format", error.message
   end
 
   def test_raises_for_invalid_date_time_format
