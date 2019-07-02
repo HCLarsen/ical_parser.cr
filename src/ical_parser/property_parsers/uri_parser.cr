3f0737319492e58c188de4e9a1e49f7df496fcb7
Chris Larsen <clarsendev@protonmail.com>
2019-07-01 21:47:07 -0400

    Revise URIParser to return string

--- src/ical_parser/property_parsers/uri_parser.cr
+++ src/ical_parser/property_parsers/uri_parser.cr
@@ -3,7 +3,7 @@ require "uri"
 require "./value_parser"
 
 module IcalParser
-  @@uri_parser = Proc(String, Hash(String, String), URI).new do |value, params|
-    URI.parse(value)
+  @@uri_parser = Proc(String, Hash(String, String), String).new do |value, params|
+    value
   end
 end
