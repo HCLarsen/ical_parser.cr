acea37f937347f2193d065851561b54d2cbd4e2b
Chris Larsen <clarsendev@protonmail.com>
2019-07-01 20:29:54 -0400

    Revise DateParser to return string

--- src/ical_parser/property_parsers/date_parser.cr
+++ src/ical_parser/property_parsers/date_parser.cr
@@ -1,19 +1,21 @@
 require "./value_parser"
 
 module IcalParser
-  @@date_parser = Proc(String, Hash(String, String), Time).new do |value, params|
+  @@date_parser = Proc(String, Hash(String, String), String).new do |value, params|
     if DATE_REGEX.match(value)
-      if tz = params["TZID"]?
-        location = Time::Location.load(tz)
-      else
-        location = Time::Location.local
-      end
+      # if tz = params["TZID"]?
+      #   location = Time::Location.load(tz)
+      # else
+      #   location = Time::Location.local
+      # end
 
       begin
-        Time.parse(value, DATE.pattern, location)
+        Time.parse(value, DATE.pattern, Time::Location.local)
       rescue
         raise "Invalid Date"
       end
+
+      value
     else
       raise "Invalid Date format"
     end
