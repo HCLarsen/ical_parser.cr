7bd6ee83292942b72f2d1053f0674fa2f306fe8f
Chris Larsen <clarsendev@protonmail.com>
2019-07-01 23:14:22 -0400

    Revise CalAddressParser to return string

--- src/ical_parser/property_parsers/cal_address_parser.cr
+++ src/ical_parser/property_parsers/cal_address_parser.cr
@@ -1,56 +1,7 @@
 require "uri"
 
 module IcalParser
-  @@caladdress_parser = Proc(String, Hash(String, String), CalAddress).new do |value, params|
-    uri = URI.parse(value)
-    address = CalAddress.new(uri)
-    address.rsvp = true if params["RSVP"]? == "TRUE"
-    address.common_name = params["CN"]?
-
-    if string = params["SENT-BY"]?
-      address.sent_by = CalAddress.new(URI.parse(string))
-    end
-
-    if string = params["MEMBER"]?
-      array = string.split(",")
-      array.each do |member|
-        member = member.strip('"')
-        address.member << CalAddress.new(URI.parse(member))
-      end
-    end
-
-    if string = params["DELEGATED-TO"]?
-      array = string.split(",")
-      array.each do |delegatee|
-        delegatee = delegatee.strip('"')
-        address.delegated_to << CalAddress.new(URI.parse(delegatee))
-      end
-    end
-
-    if string = params["DELEGATED-FROM"]?
-      array = string.split(",")
-      array.each do |delegatee|
-        delegatee = delegatee.strip('"')
-        address.delegated_from << CalAddress.new(URI.parse(delegatee))
-      end
-    end
-
-    if string = params["DIR"]?
-      address.dir = URI.parse(string.strip('"'))
-    end
-
-    if string = params["ROLE"]?
-      address.role = CalAddress::Role.parse(string)
-    end
-
-    if string = params["PARTSTAT"]?
-      address.part_stat = CalAddress::PartStat.parse(string)
-    end
-
-    if string = params["CUTYPE"]?
-      address.cutype = CalAddress::CUType.parse(string)
-    end
-
-    address
+  @@caladdress_parser = Proc(String, Hash(String, String), String).new do |value, params|
+    value
   end
 end
--- test/property_parsers/cal_address_parser_test.cr
+++ test/property_parsers/cal_address_parser_test.cr
@@ -1,11 +1,14 @@
 require "minitest/autorun"
 
-require "/../src/iCal"
+require "/../src/ical_parser/property_parsers/cal_address_parser"
+require "/../src/ical_parser/common"
+require "/../src/ical_parser/enums"
+# require "/../src/iCal"
 
 class CalAddressParserTest < Minitest::Test
   include IcalParser
 
-  @parser : Proc(String, Hash(String, String), CalAddress)
+  @parser : Proc(String, Hash(String, String), String)
 
   def initialize(arg)
     super(arg)
@@ -15,75 +18,7 @@ class CalAddressParserTest < Minitest::Test
   def test_parses_simple_address
     string = "mailto:jsmith@example.com"
     params = Hash(String, String).new
-    uri = URI.parse(string)
     address = @parser.call(string, params)
-    assert_equal uri, address.uri
-  end
-
-  def test_parses_rsvp
-    string = "mailto:jsmith@example.com"
-    params = {"RSVP" => "TRUE"}
-    address = @parser.call(string, params)
-    assert address.rsvp
-  end
-
-  def test_parses_cutype
-    string = "mailto:ietf-calsch@example.org"
-    params = {"CUTYPE" => "GROUP"}
-    address = @parser.call(string, params)
-    assert_equal CalAddress::CUType::Group, address.cutype
-  end
-
-  def test_parses_dir
-    string = "mailto:jimdo@example.com"
-    params = {"DIR" => %("ldap://example.com:6666/o=ABC%20Industries,c=US???(cn=Jim%20Dolittle)")}
-    address = @parser.call(string, params)
-    assert_equal "example.com", address.dir.not_nil!.host
-  end
-
-  def test_parses_sent_by
-    params = {"SENT-BY" => "mailto:sray@example.com"}
-    string = "mailto:jsmith@example.com"
-    address = @parser.call(string, params)
-    assert_equal "sray@example.com", address.sent_by.not_nil!.uri.path
-  end
-
-  def test_parses_members
-    string = "mailto:jsmith@example.com"
-    params = {"MEMBER" => %("mailto:projectA@example.com","mailto:projectB@example.com")}
-    address = @parser.call(string, params)
-    assert_equal 2, address.member.size
-    assert_equal "projectA@example.com", address.member.first.uri.path
-  end
-
-  def test_parses_delegated_from
-    string = "mailto:ildoit@example.com"
-    params = {"DELEGATED-FROM" => %("mailto:immud@example.com")}
-    address = @parser.call(string, params)
-    assert_equal 1, address.delegated_from.size
-    assert_equal "immud@example.com", address.delegated_from.first.uri.path
-  end
-
-  def test_parses_multiple_delegated_to
-    params = {"DELEGATED-TO" => %("mailto:jdoe@example.com","mailto:jqpublic@example.com")}
-    string = "jsmith@example.com"
-    address = @parser.call(string, params)
-    assert_equal 2, address.delegated_to.size
-    assert_equal "jdoe@example.com", address.delegated_to.first.uri.path
-  end
-
-  def test_parses_complicated_cal_address
-    params = {"ROLE" => "NON-PARTICIPANT", "PARTSTAT" => "DELEGATED", "DELEGATED-TO" => "mailto:hcabot@example.com", "CN" => "The Big Cheese"}
-    string = "mailto:iamboss@example.com"
-    address = @parser.call(string, params)
-    assert_equal "iamboss@example.com", address.uri.path
-    assert_equal "The Big Cheese", address.common_name
-
-    delegated = CalAddress.new(URI.parse("mailto:hcabot@example.com"))
-    assert_equal 1, address.delegated_to.size
-    assert_equal delegated.uri.path, address.delegated_to.first.uri.path
-
-    assert_equal CalAddress::Role::NonParticipant, address.role
-    assert_equal CalAddress::PartStat::Delegated, address.part_stat
+    assert_equal string, address
   end
 end
