d3bbc0df6599d46b16610f40140e7efc41324264
Chris Larsen <clarsendev@protonmail.com>
2019-07-07 14:02:30 -0400

    Revise Property#parse to output JSON string

--- src/ical_parser/property.cr
+++ src/ical_parser/property.cr
@@ -8,37 +8,18 @@ module IcalParser
     def initialize(@parser : ParserType, *, @alt_values = [] of String, @parts = ["value"], @only_once = true, @single_value = true)
     end
 
-    def parse(value : String, params : String?) : Hash(String, JSON::Any)
-      output = Hash(String, JSON::Any).new
-
+    def parse(value : String, params : String?) : String
       params ||= ""
       params_hash = parse_params(params)
 
-      output["params"] = JSON::Any.new(params_hash)
-      output["value"] = parse_value(value, params_hash)
-      output
+      parser = active_parser(params_hash)
+      value_hash = parse_value(value, parser)
+
+      %({"params":#{params_hash},"value":#{value_hash}})
     end
 
-    def parse_params(params : String) : Hash(String, JSON::Any)
-      output = Hash(String, JSON::Any).new
-      return output if params.empty?
-
-      params = params.lstrip(';')
-
-      array = params.split(";").map do |item|
-        pair = item.split("=")
-        if pair.size == 2
-          output[pair.first] = JSON::Any.new(pair.last)
-        else
-          raise "Invaild parameters format"
-        end
-      end
-
-      output
-    end
-
-    def parse_value(value : String, params : Hash(String, JSON::Any)) : JSON::Any
-      # new_params = Hash(String, String).new
+    def active_parser(params) : ParserType
+      params = JSON.parse(params)
 
       if value_type = params["VALUE"]?
         if @alt_values.includes?(value_type)
@@ -49,14 +30,39 @@ module IcalParser
       else
         parser = @parser
       end
+    end
+
+    def parse_params(params : String) : String
+      output = Hash(String, String | Array(String)).new
+      return output.to_json if params.empty?
+
+      params = params.lstrip(';')
+
+      array = params.split(";").map do |item|
+        pair = item.split("=")
+        if pair.size == 2
+          if ["DELEGATED-TO", "DELEGATED-FROM", "MEMBER"].includes?(pair.first)
+            output[pair.first] = pair.last.split(',').map(&.strip('"'))
+          else
+            output[pair.first] = pair.last
+          end
+        else
+          raise "Invaild parameters format"
+        end
+      end
+
+      output.to_json
+    end
+
+    def parse_value(value : String, parser : ParserType) : String
       if @single_value
         if @only_once && @parts.size == 1
           parsed = parser.call(value).as T
-          JSON::Any.new(parsed)
+          parsed.to_json
         elsif @parts.size > 1
           values = value.split(/(?<!\\);/)
           parts = values.map { |e| JSON::Any.new(parser.call(e).as T) }
-          JSON::Any.new(Hash.zip(@parts, parts))
+          Hash.zip(@parts, parts).to_json
         else
           # [parser.call(value, new_params).as T].as Array(T)
           raise "Error"
@@ -64,7 +70,7 @@ module IcalParser
       else
         values = value.split(/(?<!\\),/)
         values = values.map { |e| JSON::Any.new(parser.call(e).as T) }
-        JSON::Any.new(values)
+        values.to_json
       end
     end
   end
--- test/property_test.cr
+++ test/property_test.cr
@@ -13,27 +13,27 @@ class PropertyTest < Minitest::Test
     prop = Property(String).new(@@text_parser)
     value = "Networld+Interop Conference and Exhibit\nAtlanta World Congress Center\nAtlanta\, Georgia"
     params = {} of String => String
-    hash = {"params" => params, "value" => value}
+    json = %({"params":#{params},"value":#{value.to_json}})
     result = prop.parse(value, "")
-    assert_equal hash, result
+    assert_equal json, result
   end
 
   def test_property_parses_value_with_params
     prop = Property(String).new(@@caladdress_parser)
     params = ";RSVP=TRUE;ROLE=REQ-PARTICIPANT;CUTYPE=GROUP"
     value = "mailto:employee-A@example.com"
-    hash = {"params" => {"RSVP" => "TRUE", "ROLE" => "REQ-PARTICIPANT", "CUTYPE" => "GROUP"}, "value" => value}
+    json = %({"params":{"RSVP":"TRUE","ROLE":"REQ-PARTICIPANT","CUTYPE":"GROUP"},"value":"#{value}"})
     result = prop.parse(value, params)
-    assert_equal hash, result
+    assert_equal json, result
   end
 
   def test_single_category_returns_as_array
     prop = Property(String).new(@@text_parser, single_value: false, only_once: false)
     value = "MEETING"
     params = {} of String => String
-    hash = {"params" => params, "value" => [value]}
+    json = %({"params":#{params},"value":#{[value]}})
     result = prop.parse(value, "")
-    assert_equal hash, result
+    assert_equal json, result
   end
 
   def test_parses_geo
@@ -41,7 +41,7 @@ class PropertyTest < Minitest::Test
     prop = Property(Float64).new(@@float_parser, parts: ["lat", "lon"])
     value = "37.386013;-122.082932"
     params = {} of String => String
-    expected = {"params" => params, "value" => { "lat" => 37.386013, "lon" => -122.082932 }}
+    expected = %({"params":#{params},"value":{"lat":#{37.386013},"lon":#{-122.082932}}})
 
     result = prop.parse(value, "")
     assert_equal expected, result
@@ -51,14 +51,14 @@ class PropertyTest < Minitest::Test
     prop = Property(CalAddress).new(@@caladdress_parser)
     params = %(;DELEGATED-TO="mailto:jdoe@example.com","mailto:jqpublic@example.com")
     parsed_params = prop.parse_params(params)
-    assert_equal %("mailto:jdoe@example.com","mailto:jqpublic@example.com"), parsed_params["DELEGATED-TO"]
+    assert_equal %({"DELEGATED-TO":["mailto:jdoe@example.com","mailto:jqpublic@example.com"]}), parsed_params
   end
 
   def test_multiple_return_types
     prop = Property(String).new(@@date_time_parser, alt_values: ["DATE", "PERIOD"], single_value: false, only_once: false)
 
     value = "19970714T083000"
-    expected = {"params" => {"TZID" => "America/New_York"}, "value" => [value]}
+    expected = %({"params":{"TZID":"America/New_York"},"value":#{[value]}})
     date_time = prop.parse(value, ";TZID=America/New_York")
     assert_equal expected, date_time
   end
@@ -67,7 +67,7 @@ class PropertyTest < Minitest::Test
     prop = Property(String).new(@@date_time_parser, alt_values: ["DATE", "PERIOD"], single_value: false, only_once: false)
 
     date_value = "19970101,19970120,19970217,19970421,19970526,19970704,19970901,19971014,19971128,19971129,19971225"
-    expected = { "params" => {"VALUE" => "DATE"}, "value" =>  ["19970101","19970120","19970217","19970421","19970526","19970704","19970901","19971014","19971128","19971129","19971225"]}
+    expected = %({"params":{"VALUE":"DATE"},"value":["19970101","19970120","19970217","19970421","19970526","19970704","19970901","19971014","19971128","19971129","19971225"]})
     result = prop.parse(date_value, "VALUE=DATE")
     assert_equal expected, result
   end
