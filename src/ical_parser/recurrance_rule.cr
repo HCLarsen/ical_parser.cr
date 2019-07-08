024304711355889056e02416f22b7b182197e924
Chris Larsen <clarsendev@protonmail.com>
2019-07-07 23:26:52 -0400

    Revise RecurrenceRuleParser to output JSON string

--- src/ical_parser/recurrance_rule.cr
+++ src/ical_parser/recurrance_rule.cr
@@ -1,3 +1,5 @@
+require "json"
+
 module IcalParser
   # Representation of the Recurrence Rule.
   #
--- test/property_parsers/recurrence_rule_parser_test.cr
+++ test/property_parsers/recurrence_rule_parser_test.cr
@@ -1,11 +1,13 @@
 require "minitest/autorun"
 
-require "/../src/iCal"
+require "/../src/ical_parser/property_parsers/recurrence_rule_parser"
+require "/../src/ical_parser/common"
+require "/../src/ical_parser/enums"
 
 class RecurrenceRuleParserTest < Minitest::Test
   include IcalParser
 
-  @parser : Proc(String, Hash(String, String), RecurrenceRule)
+  @parser : Proc(String, Hash(String, String), String)
 
   def initialize(arg)
     super(arg)
@@ -16,73 +18,60 @@ class RecurrenceRuleParserTest < Minitest::Test
   def test_daily_with_count
     string = "FREQ=DAILY;COUNT=10"
     recur = @parser.call(string, @params)
-    assert_equal 10, recur.count
-    assert_equal RecurrenceRule::Freq::Daily, recur.frequency
-    assert_equal 1, recur.interval
+    assert_equal %({"freq":"daily","count":10}), recur
   end
 
   def test_daily_with_until
     string = "FREQ=DAILY;UNTIL=19971224T000000Z"
     recur = @parser.call(string, @params)
-    assert_equal Time.utc(1997, 12, 24), recur.end_time
-    assert_equal 1, recur.interval
+    assert_equal %({"freq":"daily","until":"19971224T000000Z"}), recur
   end
 
   def test_daily_with_interval
     string = "FREQ=DAILY;INTERVAL=2"
     recur = @parser.call(string, @params)
-    assert_equal RecurrenceRule::Freq::Daily, recur.frequency
-    assert_equal 2, recur.interval
+    assert_equal %({"freq":"daily","interval":2}), recur
   end
 
   def test_yearly_by_month_by_day
     string = "FREQ=YEARLY;UNTIL=20000131T140000Z;BYMONTH=1;BYDAY=SU,MO,TU,WE,TH,FR,SA"
-    sundays = {0, Time::DayOfWeek::Sunday}
     recur = @parser.call(string, @params)
-    assert_equal [1], recur.by_month
-    assert_equal sundays, recur.by_day.not_nil!.first
+    assert_equal %({"freq":"yearly","until":"20000131T140000Z","bymonth":[1],"byday":["SU","MO","TU","WE","TH","FR","SA"]}), recur
   end
 
   def test_by_positive_and_negative_days
     string = "FREQ=MONTHLY;INTERVAL=2;COUNT=10;BYDAY=1SU,-1SU"
-    first_sunday = {1, Time::DayOfWeek::Sunday}
-    last_sunday = {-1, Time::DayOfWeek::Sunday}
     recur = @parser.call(string, @params)
-    assert_equal [first_sunday, last_sunday], recur.by_day
+    assert_equal %({"freq":"monthly","interval":2,"count":10,"byday":["1SU","-1SU"]}), recur
   end
 
   def test_multiple_by_xxx_rules
     string = "FREQ=YEARLY;INTERVAL=2;BYMONTH=1;BYDAY=SU;BYHOUR=8,9;BYMINUTE=30"
     recur = @parser.call(string, @params)
-    assert_equal RecurrenceRule::Freq::Yearly, recur.frequency
-    assert_equal 2, recur.interval
-    assert_equal [1], recur.by_month
-    assert_equal [{0, Time::DayOfWeek::Sunday}], recur.by_day
-    assert_equal [8, 9], recur.by_hour
-    assert_equal [30], recur.by_minute
+    assert_equal %({"freq":"yearly","interval":2,"bymonth":[1],"byday":["SU"],"byhour":[8,9],"byminute":[30]}), recur
   end
 
   def test_yearly_by_weekno
     string = "FREQ=YEARLY;BYWEEKNO=20;BYDAY=MO"
     recur = @parser.call(string, @params)
-    assert_equal [20], recur.by_week
+    assert_equal %({"freq":"yearly","byweekno":[20],"byday":["MO"]}), recur
   end
 
   def test_by_month_day
     string = "FREQ=MONTHLY;BYMONTHDAY=-3"
     recur = @parser.call(string, @params)
-    assert_equal [-3], recur.by_month_day
+    assert_equal %({"freq":"monthly","bymonthday":[-3]}), recur
   end
 
   def test_by_year_day
     string = "FREQ=YEARLY;INTERVAL=3;COUNT=10;BYYEARDAY=1,100,200"
     recur = @parser.call(string, @params)
-    assert_equal [1, 100, 200], recur.by_year_day
+    assert_equal %({"freq":"yearly","interval":3,"count":10,"byyearday":[1,100,200]}), recur
   end
 
   def test_set_pos
     string = "FREQ=MONTHLY;BYDAY=MO,TU,WE,TH,FR;BYSETPOS=-2"
     recur = @parser.call(string, @params)
-    assert_equal [-2], recur.by_set_pos
+    assert_equal %({"freq":"monthly","byday":["MO","TU","WE","TH","FR"],"bysetpos":[-2]}), recur
   end
 end
