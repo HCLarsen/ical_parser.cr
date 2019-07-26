ceebe5bc6ea4613c9767e8358d23d2940521f4b4
Chris Larsen <clarsendev@protonmail.com>
2019-07-25 23:25:31 -0400

    Add StreamParser folder to requires

--- src/iCal.cr
+++ src/iCal.cr
@@ -3,6 +3,7 @@ require "./ical_parser/*"
 require "./ical_parser/components/*"
 require "./ical_parser/component_parsers/*"
 require "./ical_parser/property_parsers/*"
+require "./ical_parser/stream_parser/*"
 
 # TODO: Write documentation for `IcalParser`
 module IcalParser
