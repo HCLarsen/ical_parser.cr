fbedb5f62e2c33aae2e1a1284e5f3ca0eabfac4e
Chris Larsen <clarsendev@protonmail.com>
2019-09-02 19:58:10 -0400

    Move constants from iCal to common

--- src/ical_parser/common.cr
+++ src/ical_parser/common.cr
@@ -16,4 +16,55 @@ module IcalParser
 
   DUR_DATE_REGEX  = /^(?<polarity>[+-])?P((?<days>\d+)D)?(T((?<hours>\d+)H)?((?<minutes>\d+)M)?((?<seconds>\d+)S)?)?$/
   DUR_WEEKS_REGEX = /^(?<polarity>[+-])?P(?<weeks>\d+)W$/
+
+  alias ParserType = Proc(String, Hash(String, String), String)
+
+  PARSERS = {
+    "BINARY"      => @@text_parser,  # To be replaced with BinaryParser once written.
+    "BOOLEAN"     => @@boolean_parser,
+    "CAL-ADDRESS" => @@caladdress_parser,
+    "DATE"        => @@date_parser,
+    "DATE-TIME"   => @@date_time_parser,
+    "DURATION"    => @@duration_parser,
+    "FLOAT"       => @@float_parser,
+    "INTEGER"     => @@integer_parser,
+    "PERIOD"      => @@period_parser,
+    "RECUR"       => @@recurrence_parser,
+    "TEXT"        => @@text_parser,
+    "TIME"        => @@time_parser,
+    "URI"         => @@uri_parser,
+    "UTC-OFFSET"  => @@text_parser,  # To be replaced with UTCOffsetParser once written.
+  }
+
+  COMPONENT_PROPERTIES = {
+    uid: {types: ["TEXT"], required: true},
+    dtstamp: {types: ["DATE-TIME"], required: true},
+    created: {types: ["DATE-TIME"]},
+    last_modified: {types: ["DATE-TIME"]},
+    dtstart: {types: ["DATE-TIME", "DATE"], required: true},
+    dtend: {types: ["DATE-TIME", "DATE"]},
+    duration: {types: ["DURATION"]},
+    summary: {types: ["TEXT"]},
+    class: {types: ["TEXT"], default: "public"},
+    categories: {types: ["TEXT"], list: true},
+    resources: {types: ["TEXT"], list: true},
+    contact: {types: ["TEXT"]},
+    related_to: {types: ["TEXT"], key: "related-to", getter: false},
+    request_status: {types: ["TEXT"], key: "request-status", getter: false},
+    transparency: {types: ["TEXT"], default: "opaque"},
+    description: {types: ["TEXT"]},
+    status: {types: ["TEXT"]},
+    comment: {types: ["TEXT"]},
+    location: {types: ["TEXT"]},
+    priority: {types: ["INTEGER"]},
+    sequence: {types: ["INTEGER"]},
+    organizer: {types: ["CAL-ADDRESS"]},
+    attendee: {types: ["CAL-ADDRESS"]},
+    geo: {types: ["FLOAT"]},
+    rrule: {types: ["RECUR"]},
+    exdate: {types: ["DATE-TIME", "DATE"], list: true},
+    rdate: {types: ["DATE-TIME", "DATE", "PERIOD"], list: true},
+    url: {types: ["URI"], converter: URI::URIConverter},
+    all_day: {types: ["BOOLEAN"], key: "all-day"}
+  }
 end
